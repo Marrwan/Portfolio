f2218b0812c3fb61db35cfc2784329e8983c0e29
Abuabdurrahman <abdulbasitdamilola6@gmail.com>
2021-07-24 11:42:20 +0100

    organize the code

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,16 @@ export default function About() {
   return (
     <section id="about" className="text-gray-400 bg-gray-900 body-font">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+       <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center" >
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+
+
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6" >
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Abdulbasit.
             <br className="hidden lg:inline-block" />I love to build amazing
@@ -29,13 +38,7 @@ export default function About() {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-          <img
-            className="object-cover object-center rounded"
-            alt="hero"
-            src="./coding.svg"
-          />
-        </div>
+       
       </div>
     </section>
   );
